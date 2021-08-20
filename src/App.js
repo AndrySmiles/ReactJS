e15142a02a17c87bd754fd54ce0ab801ad5946bf
AndrySmiles <caramu@yandex.ru>
2021-08-20 12:27:38 +0300

    Переписал весь код ДЗ

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-
-import './App.css';
-import Message from './Components/Message';
-import MessageList from './Components/MessageList';
-
+import React from 'react';
+import { useState } from 'react'
+import MessageList from './components/MessageList'
+import MessageListForm from './components/MessageListForm'
 
 function App() {
+  const [msg, setMsg] = useState([])
+
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2, 9),
+        task: userInput,
+        authot: 'user'
+      }
+      setMsg([...msg, newItem])
+    }
+  }
+
   return (
     <div className="App">
-      <Message author="author" />
-      <MessageList />
-      <index />
-
+      <header>
+        <h4>Количество сообщений: {msg.length}</h4>
+      </header>
+      <MessageListForm addTask={addTask} />
+      {msg.map((message) => {
+        return (
+          <MessageList
+            message={message}
+            key={message.id}
+          />
+        )
+      })}
     </div>
   );
 }
